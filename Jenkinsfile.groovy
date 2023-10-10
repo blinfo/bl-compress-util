d880fe9967ab6c074f3e6e4c78131e821028588b
hakan <hakan@NL579>
2023-10-10 09:58:19 +0200

    master updated Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,18 +7,18 @@ pipeline {
         stage('Build and deploy artifact'){
             agent {
                 docker {
-                    image 'blinfo/maven:3.6.3-jdk-13'
+                    image 'docker.repos.blinfo.se/blinfo/maven:3.8.5-jdk-17'
                     args '-u root'
                 }
             }
             steps {
                 sh "mvn clean package deploy -DskipITs"
             }
-        }   
+        }
     }
     post {
         failure {
             emailext body: '${DEFAULT_CONTENT}', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: '${DEFAULT_SUBJECT}'
-        }
+        }  
     }
 }
